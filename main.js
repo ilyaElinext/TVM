ff7637abb2018476f8d29f035c851b175f78141f
ilya yasiuk <iyasiuk@elinext.com>
2021-03-04 23:00:28 +0300

    v

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { autoUpdater } = require('electron-updater');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { autoUpdater } = require("electron-updater");
 
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 800,
-    show : false, 
+    show: false,
+    fullscreen: true,
+    autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
     },
   });
-  mainWindow.loadFile('index.html');
-  mainWindow.on('closed', function () {
+  mainWindow.loadFile("index.html");
+  mainWindow.on("closed", function () {
     mainWindow = null;
   });
-  mainWindow.once('ready-to-show', () => {
+  mainWindow.once("ready-to-show", () => {
     mainWindow.show();
     // setTimeout(()=>{
     //   mainWindow.close();
@@ -24,38 +26,46 @@ function createWindow () {
     autoUpdater.checkForUpdatesAndNotify();
   });
   mainWindow.on("maximize", () => {
-    mainWindow.close();
+    // mainWindow.close();
+    app.relaunch({ args: process.argv.slice(1).concat(["--relaunch"]) });
+    app.exit(0);
   });
 }
 
-app.on('ready', () => {
+app.on("ready", () => {
   createWindow();
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', function () {
+app.on("activate", function () {
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
 });
 
-autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update_available');
+autoUpdater.on("update-available", () => {
+  mainWindow.webContents.send("update_available");
 });
 
-autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
+autoUpdater.on("update-downloaded", () => {
+  mainWindow.webContents.send("update_downloaded");
 });
 
-ipcMain.on('restart_app', () => {
+ipcMain.on("restart_app", () => {
   autoUpdater.quitAndInstall();
+});
+
+ipcMain.on("relaunch_app", () => {
+  autoUpdater.checkForUpdatesAndNotify();
+  // app.relaunch({ args: process.argv.slice(1).concat(["--relaunch"]) });
+  // app.exit(0);
 });
